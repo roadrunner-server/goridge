9530a18b1719900eb00080d774068f058174063c
Wolfy-J <wolfy.jd@gmail.com>
2018-06-03 17:46:04 +0300

    fixing tests

--- tests/Cases/RPCTest.php
+++ tests/Cases/RPCTest.php
@@ -56,7 +56,7 @@ abstract class RPCTest extends TestCase
 
     /**
      * @expectedException \Spiral\Goridge\Exceptions\ServiceException
-     * @expectedExceptionMessageRegExp #error '{rawData} request for <*string Value>'.*#
+     * @expectedExceptionMessage {rawData} request for <*string Value>
      */
     public function testConvertException()
     {
@@ -120,7 +120,7 @@ abstract class RPCTest extends TestCase
 
     /**
      * @expectedException \Spiral\Goridge\Exceptions\ServiceException
-     * @expectedExceptionMessageRegExp #error '{rawData} request for <*main.Payload Value>.*#
+     * @expectedExceptionMessage {rawData} request for <*main.Payload Value>
      */
     public function testBadPayload()
     {
