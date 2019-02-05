5ea54ce09f65647c316e29f2528d08537e07116c
Wolfy-J <wolfy.jd@gmail.com>
2019-02-05 16:06:13 +0300

    Update client.php

--- examples/client.php
+++ examples/client.php
@@ -1,9 +1,10 @@
 <?php
-
 use Spiral\Goridge;
 
 require "vendor/autoload.php";
 
-$rpc = new Goridge\RPC(new Goridge\SocketRelay("127.0.0.1", 6001));
+$rpc = new Goridge\RPC(
+  new Goridge\SocketRelay("127.0.0.1", 6001)
+);
 
 echo $rpc->call("App.Hi", "Antony");
