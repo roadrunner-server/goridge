d002784105dccf81cdc873f72ee487b324dff9fe
Anton Titov <wolfy.jd@gmail.com>
2019-11-30 12:21:35 +0300

    Update StreamRelay.php

--- php-src/StreamRelay.php
+++ php-src/StreamRelay.php
@@ -140,7 +140,7 @@ class StreamRelay implements RelayInterface
     {
         $meta = stream_get_meta_data($stream);
 
-        return in_array($meta['mode'], ['r', 'r+', 'w+', 'a+', 'x+', 'c+'], true);
+        return in_array($meta['mode'], ['r', 'rb', 'r+', 'w+', 'a+', 'x+', 'c+'], true);
     }
 
     /**
