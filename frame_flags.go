95a74ff90f0dc6a9ea518f4484eba86247c00d80
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-12-10 21:42:13 +0300

    Add CONTROL flag instead from CONTEXT_SEPARATOR

--- frame_flags.go
+++ frame_flags.go
@@ -5,13 +5,13 @@ type FrameFlag byte
 // BYTE flags, it means, that we can set multiply flags from this group using bitwise OR
 // For example CONTEXT_SEPARATOR | CODEC_RAW
 const (
-	CONTEXT_SEPARATOR FrameFlag = 0x01 //nolint:stylecheck,golint
-	CODEC_RAW         FrameFlag = 0x04 //nolint:stylecheck,golint
-	CODEC_JSON        FrameFlag = 0x08 //nolint:stylecheck,golint
-	CODEC_MSGPACK     FrameFlag = 0x10 //nolint:stylecheck,golint
-	CODEC_GOB         FrameFlag = 0x20 //nolint:stylecheck,golint
-	ERROR             FrameFlag = 0x40 //nolint:stylecheck,golint
-	RESERVED1         FrameFlag = 0x80 //nolint:stylecheck,golint
+	CONTROL       FrameFlag = 0x01 //nolint:stylecheck,golint
+	CODEC_RAW     FrameFlag = 0x04 //nolint:stylecheck,golint
+	CODEC_JSON    FrameFlag = 0x08 //nolint:stylecheck,golint
+	CODEC_MSGPACK FrameFlag = 0x10 //nolint:stylecheck,golint
+	CODEC_GOB     FrameFlag = 0x20 //nolint:stylecheck,golint
+	ERROR         FrameFlag = 0x40 //nolint:stylecheck,golint
+	RESERVED1     FrameFlag = 0x80 //nolint:stylecheck,golint
 )
 
 // COMPLEX flags can't be used with Byte flags, because it's value more than 128
